f609ea169be2e11691f737a0df899c7eec3c3271
Ahmed Qureshi <aqureshi@Virtusa.com>
2019-10-23 17:03:24 -0400

    Upgraded table to Material Table and added pagination and filtering

--- src/app/Component/people-table/people-table.component.ts
+++ src/app/Component/people-table/people-table.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { PeopleService } from '../../Service/people.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
+
+/**
+ * @title Table grabbing people information.
+ */
 @Component({
   selector: 'app-people-table',
   templateUrl: './people-table.component.html',
@@ -8,13 +15,25 @@ import { PeopleService } from '../../Service/people.service';
 })
 export class PeopleTableComponent implements OnInit {
 
-  people: any = [];
-
   constructor(private peopleService: PeopleService) { }
 
+  key = 'results';
+  displayedColumns: string[] = ['name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year', 'gender'];
+
+  dataSource = new MatTableDataSource();
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
   ngOnInit() {
-    // tslint:disable-next-line: no-string-literal
-    this.peopleService.getAllPeople().subscribe(res => this.people = res['results']);
+    this.peopleService.getAllPeople().subscribe(res => this.dataSource.data = res[this.key]);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material';
+import { MatTableModule } from '@angular/material'
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+
 
 import { HomeComponent } from './Component/home/home.component';
 import { PageNotFoundComponent } from './Component/page-not-found/page-not-found.component';
 import { PeopleService } from './Service/people.service';
 import { PeopleTableComponent } from './Component/people-table/people-table.component';
+
+
 
 
 @NgModule({
@@ -24,7 +34,14 @@ import { PeopleTableComponent } from './Component/people-table/people-table.comp
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatTabsModule,
+    MatTableModule,
+    MatSortModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [PeopleService],
   bootstrap: [AppComponent]
